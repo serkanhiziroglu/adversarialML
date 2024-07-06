04f98acd0fb808ec53fc9b33414e527f2a429689
Serkan Hiziroglu <71054911+serkanhiziroglu@users.noreply.github.com>
2024-07-05 17:40:04 -0700

    File drop menu improved

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,9 +1,41 @@
 export default function FAQ() {
     return (
-        <div className="flex flex-col flex-center ">
-            <div className="max-w-4xl mx-auto px-4 py-8">
-                <h1 className="text-3xl font-bold ">Frequently Asked Questions</h1>
-                {/* Add your FAQ content here */}
+        <div className="flex flex-col items-center">
+            <div className="max-w-4xl mx-auto px-4 py-8 text-center">
+                <h1 className="text-3xl font-bold">Frequently Asked Questions</h1>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">What is Adversarial Image Protection?</h2>
+                    <p className="mt-2">Adversarial Image Protection is a method to strengthen your images against AI-based adversarial attacks, ensuring that they are secure and resistant to unauthorized manipulation or analysis.</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">How do I protect my images online?</h2>
+                    <p className="mt-2">Protecting your images online is simple:</p>
+                    <ol className="list-decimal list-inside mt-2">
+                        <li>Upload an image: Click on the "Upload your image" button or drag and drop your file. Our system supports various image formats.</li>
+                        <li>Apply protection: Our system will automatically apply adversarial protection to your image, enhancing its security.</li>
+                        <li>Download protected image: Once processed, download your high-resolution, protected image ready for safe sharing or online use.</li>
+                    </ol>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">What image formats are supported?</h2>
+                    <p className="mt-2">Our system accepts a wide range of image formats including JPEG, PNG, BMP, and GIF.</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">Is there a limit to the number of images I can protect?</h2>
+                    <p className="mt-2">No, you can protect as many images as you need. There are no limits on the number of images you can upload and protect.</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">Will the quality of my image be affected?</h2>
+                    <p className="mt-2">No, the quality of your image will remain high. The protection process ensures that your image is secure without compromising on resolution or quality.</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">How long does it take to process an image?</h2>
+                    <p className="mt-2">The processing time is typically very short, usually taking just a few seconds to apply the protection and make the image ready for download.</p>
+                </div>
+                <div className="mt-6">
+                    <h2 className="text-xl font-semibold">Can I use the protected images on social media?</h2>
+                    <p className="mt-2">Yes, the protected images can be safely shared on social media platforms.</p>
+                </div>
             </div>
         </div>
     )
--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -44,10 +44,7 @@ const UploadForm = () => {
                 },
             });
 
-            // Store the response data in localStorage
             localStorage.setItem('resultData', JSON.stringify(response.data));
-
-            // Redirect to the results page
             router.push('/result');
         } catch (error) {
             console.error('Error uploading file:', error);
@@ -60,45 +57,51 @@ const UploadForm = () => {
         <div className="max-w-6xl mx-auto mt-8">
             <div
                 className="shadow-lg rounded-xl overflow-hidden"
-                style={{ backgroundImage: 'url(/form-bg-4.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+                style={{ backgroundImage: 'url(/form-bg-3.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
             >
                 <div className="px-8 py-12 sm:px-12 sm:py-16">
-                    <div className="bg-white bg-opacity-80 backdrop-blur-sm backdrop-filter flex items-center flex-col border border-gray-200 rounded-xl px-8 py-6 max-w-3xl mx-auto">
-                        <h2 className="text-3xl font-bold mb-2">Protect your images</h2>
-                        <p className="text-gray-600 mb-6 text-center">
-                            Strengthen your images against adversarial attacks with our easy-to-use protection tools. Upload an image and get your protected image.
-                        </p>
-                        <div className="flex flex-col items-center">
-                            <input
-                                type="file"
-                                id="file"
-                                accept="image/*"
-                                onChange={handleFileChange}
-                                className="hidden"
-                            />
-                            <label
-                                htmlFor="file"
-                                className="mb-2 inline-flex items-center justify-center px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 cursor-pointer"
-                            >
-                                <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z" />
-                                    <path d="M9 13h2v5a1 1 0 11-2 0v-5z" />
-                                </svg>
-                                Upload your image
-                            </label>
-                            <p className="text-sm text-gray-500 mb-2">or drop it here</p>
-                            {file && <p className="text-sm text-gray-600 mb-2">Selected file: {file.name}</p>}
-                            {error && <p className="text-sm text-red-600 mb-2">{error}</p>}
-                            <button
-                                onClick={handleSubmit}
-                                className="px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-                            >
-                                Protect Image
-                            </button>
+                    <div className="bg-white bg-opacity-80 backdrop-blur-sm backdrop-filter flex items-center justify-center border border-gray-200 rounded-xl px-6 py-4 max-w-3xl mx-auto transition-all duration-300 hover:bg-opacity-90 group relative">
+                        {/* Extra div for dotted line */}
+                        <div className="absolute inset-2 border border-dashed border-transparent group-hover:border-gray-300 rounded-lg pointer-events-none"></div>
+
+                        <div className="w-full h-full flex flex-col items-center justify-center z-10 px-4 py-8">
+                            <h2 className="text-3xl font-bold mb-2">Protect your images</h2>
+                            <p className="text-gray-600 mb-6 text-center">
+                                Strengthen your images against adversarial attacks with our easy-to-use protection tools. Upload an image and get your protected image.
+                            </p>
+                            <div className="flex flex-col items-center">
+                                <input
+                                    type="file"
+                                    id="file"
+                                    accept="image/*"
+                                    onChange={handleFileChange}
+                                    className="hidden"
+                                />
+                                <label
+                                    htmlFor="file"
+                                    className="mb-2 inline-flex items-center justify-center px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-teal-400 hover:from-blue-600 hover:to-teal-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 cursor-pointer"
+                                >
+                                    <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z" />
+                                        <path d="M9 13h2v5a1 1 0 11-2 0v-5z" />
+                                    </svg>
+                                    Upload your image
+                                </label>
+                                <p className="text-sm text-gray-500 mb-2">or drop it here</p>
+                                {file && <p className="text-sm text-gray-600 mb-2">Selected file: {file.name}</p>}
+                                {error && <p className="text-sm text-red-600 mb-2">{error}</p>}
+                                <button
+                                    onClick={handleSubmit}
+                                    className="px-6 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+                                >
+                                    Protect Image
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
             {loading && (
                 <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center">
                     <div className="bg-white p-6 rounded-lg shadow-xl">
